e34d23b564542bf3e07e2c69ce202f6cd0a15c24
Gabriel Pereyra <gabrielp1689@gmail.com>
2021-12-11 20:07:26 -0300

    method getByPrioridad added

--- User/src/main/java/com/User/repository/UserRepository.java
+++ User/src/main/java/com/User/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.User.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
-    
+    public abstract ArrayList<UserEntity> findByPrioridad(Integer prioridad);
 }
