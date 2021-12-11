ec3d01fa6bcdd5c94151f2391c8e784d8970b09c
Gabriel Pereyra <gabrielp1689@gmail.com>
2021-12-11 20:29:59 -0300

    new methods added (delete, getByID, getByPrioridad)

--- User/src/main/java/com/User/controller/UserController.java
+++ User/src/main/java/com/User/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -20,6 +21,27 @@ public class UserController {
 
     @PostMapping()
     public UserEntity saveUser(@RequestBody UserEntity userEntity){
-        return userService.saveUser(userEntity);
+        return this.userService.saveUser(userEntity);
     }
+
+    @GetMapping("/{id}")
+    public Optional<UserEntity> getUserById(@PathVariable("id") Long id){
+        return this.userService.getById(id);
+    }
+
+    @GetMapping("/query")
+        public ArrayList<UserEntity> getUserByPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return this.userService.getByPrioridad(prioridad);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteById(@PathVariable("id") Long id){
+        boolean status = this.userService.deleteUser(id);
+        if(status){
+            return "Se eliminó el usuario con el ID: "+id;
+        } else{
+            return "No se pudo eliminar el usuario con el ID: "+id;
+        }
+    }
+
 }
