28c865d366702cc4bab4e4c081856f1c30ec9881
Gabriel Pereyra <gabrielp1689@gmail.com>
2021-12-11 20:29:17 -0300

    new methods added (delete, getByID, getByPrioridad)

--- User/src/main/java/com/User/service/UserService.java
+++ User/src/main/java/com/User/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -20,5 +21,23 @@ public class UserService {
     public UserEntity saveUser(UserEntity userEntity){
         return userRepository.save(userEntity);
         //Regresa el usuario que guardó junto a su ID
+    }
+
+    public Optional<UserEntity> getById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public ArrayList<UserEntity> getByPrioridad(Integer prioridad){
+        return userRepository.findByPrioridad(prioridad);
+    }
+
+    public boolean deleteUser(Long id){
+        try {
+            userRepository.deleteById(id);
+            return true;
+        } catch (Exception e){
+
+            return false;
+        }
     }
 }
