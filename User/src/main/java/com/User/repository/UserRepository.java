5d8b1689d02976ef55abb3512c6339326fe9c68b
Gabriel Pereyra <gabrielp1689@gmail.com>
2021-12-11 19:34:10 -0300

    Repository added

---
+++ User/src/main/java/com/User/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.User.repository;
+
+import com.User.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    
+}
